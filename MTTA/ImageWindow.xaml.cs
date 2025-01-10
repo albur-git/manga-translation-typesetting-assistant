ac71c01aeaeab6ddd370ac6dea1ebd42649baf08
albur <albur.git@gmail.com>
2025-01-10 18:39:35 +0100

    Add simple image viewer and basic OCR

---
+++ MTTA/ImageWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MTTA
+{
+    /// <summary>
+    /// Interaction logic for ImageWindow.xaml
+    /// </summary>
+    public partial class ImageWindow : Window
+    {
+        private string _filePath;
+        private BitmapImage _originalImage;
+
+        public ImageWindow(string filePath)
+        {
+            InitializeComponent();
+
+            _filePath = filePath;
+
+            ResizeImage();
+        }
+
+        // This method is called when the window has been fully loaded
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Load the image (you can replace the path with a file picker or any logic)
+            _originalImage = new BitmapImage(new Uri(_filePath));
+            ImageViewer.Source = _originalImage;
+
+            // Resize the image once the window is loaded
+            ResizeImage();
+        }
+
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            ResizeImage();
+        }
+
+        private void ResizeImage()
+        {
+            if (_originalImage != null)
+            {
+                // Maintain the image's aspect ratio (width and height)
+                double aspectRatio = _originalImage.PixelWidth / (double)_originalImage.PixelHeight;
+
+                // Set the width to the window's width (minus a small margin), and calculate the height based on the aspect ratio
+                double newWidth = this.ActualWidth - 20; // Subtracting 20 for margin (adjust as needed)
+                double newHeight = newWidth / aspectRatio;
+
+                // Apply the calculated width and height
+                ImageViewer.Width = newWidth;
+                ImageViewer.Height = newHeight;
+            }
+        }
+    }
+}
--- MTTA/MainWindow.xaml.cs
+++ MTTA/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Text;
 using System.Windows;
 using System.Windows.Controls;
+using System.Drawing;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -8,6 +9,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
+using Tesseract;
 
 namespace MTTA
 {
@@ -19,6 +22,69 @@ namespace MTTA
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void buttonLoadImage_Click(object sender, RoutedEventArgs e)
+        {
+            // Create and configure the OpenFileDialog
+            var openFileDialog = new OpenFileDialog
+            {
+                Filter = "Image Files|.jpg;.jpeg;.png;.bmp;.gif|All Files|.*",  // Image file filter
+                Title = "Select an Image"
+            };
+
+            // Show the dialog and check if the user selects a file
+            if (openFileDialog.ShowDialog() == true)
+            {
+                // Get the selected file path
+                string selectedFilePath = openFileDialog.FileName;
+
+                PerformOCR(selectedFilePath);
+
+                // Create a new window and display the image
+                var newWindow = new ImageWindow(selectedFilePath);
+                newWindow.Show();
+            }
+        }
+
+        private void PerformOCR(string imagePath)
+        {
+            try
+            {
+                // Initialize the Tesseract OCR engine
+                // Note: Initializing the tesseract engine needs training data from the official project github:
+                // --> https://github.com/tesseract-ocr/tessdoc/blob/main/Data-Files.md
+                //      e.g. tessdata_fast
+                // a tessdata folder needs to be created in the project folder, the contents of above above file) 
+                // copied into and then the folder needs to be referenced in the constructor (see: @"./tessdata")
+                // keep the following files and set their property to "copy always" (don't know about the scripts and config yet)
+                // (Config just seems to point to the config folder, which is empty...)
+                // - tessdata/eng.traineddata
+                // - tessdata/equ.traineddata
+                // - tessdata/jpn.traineddata
+                // - tessdata/jpn_vert.traineddata
+                // - tessdata/osd.traineddata
+                // See https://github.com/charlesw/tesseract/ --> "Getting started quickly"
+                // See https://stackoverflow.com/questions/38567100/failed-to-initialise-tesseract-engine-cant-find-correct-version for copy always setting
+
+                // Todo: Add the Tesseract.Drawing NuGet package to support interop with System.Drawing in .NET Core, for instance to allow passing Bitmap to Tesseract
+                using (var engine = new TesseractEngine(@"./tessdata", "eng", EngineMode.Default))
+                {
+                    using (var img = Pix.LoadFromFile(imagePath))
+                    {
+                        // Perform OCR and get the text result
+                        var result = engine.Process(img);
+                        string ocrText = result.GetText();
+
+                        // Show the OCR result in a MessageBox
+                        MessageBox.Show($"OCR Result:\n\n{ocrText}", "OCR Result", MessageBoxButton.OK, MessageBoxImage.Information);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error during OCR process: {ex.Message}", "OCR Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
     }
 }
